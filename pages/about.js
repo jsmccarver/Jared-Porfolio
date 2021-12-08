4384012bef082ed76ba9a6ac2fdb702a4900a686
jmccarver <jmccarver@CAD-1Y930M2-JGH.stewarteng.local>
2021-12-08 15:42:27 -0500

    More updates for Next JS

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+function about() {
+  return (
+    <div className={"my-bio"}>
+      <p>
+        I am an aspiring web developer with 2 years of experience with React Js
+        and general web development. I graduated from North Carolina State
+        University in May 2021 with degrees in Electrical Engineering and
+        Computer Engineering.
+      </p>
+    </div>
+  );
+}
+export default about;
---
+++ pages/projects/weather-app.js
@@ -0,0 +1,8 @@
+function WeatherApp() {
+  return (
+    <div>
+      <h1>Weather App</h1>
+    </div>
+  );
+}
+export default WeatherApp;
