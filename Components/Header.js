5142200818e708d7703575758aac5fdda51e6371
jmccarver <jmccarver@CAD-1Y930M2-JGH.stewarteng.local>
2021-12-08 15:41:21 -0500

    Majority Conversion to Next JS

---
+++ Components/Header.js
@@ -0,0 +1,33 @@
+import Styles from "./styles/Header.module.css";
+import Link from "next/link";
+function Header() {
+  return (
+    <div className={Styles.navbar}>
+      <div className={Styles["Header-Container"]}>
+        <Link href="./">
+          <h1>Jared McCarver</h1>
+        </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link href="./about">
+                <h3>About</h3>
+              </Link>
+            </li>
+            <li>
+              <Link href="./projects">
+                <h3>projects</h3>
+              </Link>
+            </li>
+            <li>
+              <Link href="./contact">
+                <h3>contact</h3>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+export default Header;
