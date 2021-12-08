8a1efcbb40519e298a196a58f6e884f64f67e165
jmccarver <jmccarver@CAD-1Y930M2-JGH.stewarteng.local>
2021-12-08 16:54:36 -0500

    Hiding Header

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,13 +1,17 @@
+import { Fragment } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { useRouter } from "next/router";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const showHeader = router.pathname === "/projects/weather-app" ? false : true;
   return (
-    <div>
-      <Header />
+    <Fragment>
+      {showHeader && <Header />}
       {children}
       <Footer />
-    </div>
+    </Fragment>
   );
 };
 
