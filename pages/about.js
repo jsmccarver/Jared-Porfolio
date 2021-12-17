b90b243a5d962e505b6241b0fae8b4a655b6928e
Jared McCarver <65602997+jsmccarver@users.noreply.github.com>
2021-12-17 09:10:52 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,8 @@ function about() {
   return (
     <div className={"my-bio"}>
       <p>
-        I am an aspiring web developer with 2 years of experience with React Js
-        and general web development. I graduated from North Carolina State
+        I am an aspiring Software Engineer with 2 years of experience with React Js
+        and web development. I graduated from North Carolina State
         University in May 2021 with degrees in Electrical Engineering and
         Computer Engineering.
       </p>
