09e9fe30b15f52524efcbb13c2f8980d8b40d903
jsmccarver <jared.mccarver@gmail.com>
2022-01-04 08:51:34 -0500

    Trying to add GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
+import TagManager from "react-gtm-module";
+
 function MyApp({ Component, pageProps }) {
+  const tagManagerArgs = {
+    gtmId: "GTM-298284660",
+  };
+  TagManager.initialize(tagManagerArgs);
   return (
     <Layout>
       <Component {...pageProps} />
