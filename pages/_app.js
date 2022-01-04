63a4d7263b1975e6e4a09fcb34b3e98a527d129d
jsmccarver <jared.mccarver@gmail.com>
2022-01-04 08:59:00 -0500

    revert

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
-import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }) {
-  const tagManagerArgs = {
-    gtmId: "GTM-298284660",
-  };
-  TagManager.initialize(tagManagerArgs);
   return (
     <Layout>
       <Component {...pageProps} />
