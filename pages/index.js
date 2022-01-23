30dd5243fb3c02ff1bbf9a1a010eb328f89283f4
jsmccarv <jsmccarv@ncsu.edu>
2022-01-23 17:13:34 -0500

    Adjusting Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Styles from "../styles/Home.module.css";
 import ProjectList from "../Components/ProjectList";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
@@ -13,18 +14,23 @@ export default function Home() {
       <main>
         <div className={Styles.Home}>
           <div className={Styles.Intro}>
-            <div className={Styles["code-image"]}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M14.568.075c2.202 1.174 5.938 4.883 7.432 6.881-1.286-.9-4.044-1.657-6.091-1.179.222-1.468-.185-4.534-1.341-5.702zm7.432 10.925v13h-20v-24h8.409c4.857 0 3.335 8 3.335 8 3.009-.745 8.256-.419 8.256 3zm-11 1l-5 2.201v1.599l5 2.2v-1.653l-3.229-1.347 3.229-1.351v-1.649zm7 2.201l-5-2.201v1.649l3.229 1.351-3.229 1.347v1.653l5-2.201v-1.598z" />
-              </svg>
-            </div>
             <div className={Styles["Intro-text"]}>
-              <h2>Hello!</h2>
-              <h3>
-                My name is Jared and I am an aspiring web developer. I currently
-                work at Stewart Engineers where I am developing a Web App, using
-                React JS, to model glass coatings.
-              </h3>
+              <div>
+                <h1>I will design and develop your Websites and Apps</h1>
+                <h3>
+                  Hi, My name is Jared I am an aspiring web developer. I will
+                  work diligently using my skills to build the best website for
+                  you!
+                </h3>
+                <Link href="/#projects">
+                  <a>
+                    <button>View My Work</button>
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <img src="./Work_Home.svg"></img>
+              </div>
             </div>
           </div>
           <div id="projects">
