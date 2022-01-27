21982709b26d8cf5e6df2315c2e85f10714cc721
jsmccarver <jared.mccarver@gmail.com>
2022-01-27 11:13:08 -0500

    Add Bad looking wavy stuff

--- Components/ProjectList.js
+++ Components/ProjectList.js
@@ -9,8 +9,9 @@ const ProjectList = () => {
       Color: "#82D4BB",
       TextColor: "white",
       img: "./Allyson_site.jpg",
+      wave: "spacer spacer2",
       Description:
-        "I was the developer for Allyson&apos;s personal website. She is agraduate of NC State with a Bachelors in Geology. She is currentlypursuing a certificate in Geospatial Information Systems from NCState and does research with The Machado Lab. You can find out more through the link. The project is written in React JS.",
+        "I was the developer for Allyson's personal website. She is agraduate of NC State with a Bachelors in Geology. She is currentlypursuing a certificate in Geospatial Information Systems from NCState and does research with The Machado Lab. You can find out more through the link. The project is written in React JS.",
     },
     {
       Name: "Freeman Pet Shop",
@@ -18,6 +19,7 @@ const ProjectList = () => {
       Programs: [{ name: "Django" }, { name: "HTML5" }, { name: "Postgres" }],
       Color: "#edf2fb",
       TextColor: "#89608E",
+      wave: "spacer spacer4",
       img: "./Under-Construction-Sign.png",
       Description:
         "My Fiance and and I love pets and recently adopted a new kitten from Petsmart. I made this theoretical website in order for people to find forever homes for their fostered kittens and dogs. It is made using Django that connects to a postgres database.",
@@ -28,6 +30,7 @@ const ProjectList = () => {
       Programs: [{ name: "React" }, { name: "next" }],
       Color: "#eddcd2",
       TextColor: "#7E7F9A",
+      wave: "spacer spacer5",
       img: "./Jared_site.png",
       Description:
         "This is my personal webpage. I created it using React and Next JS. ",
@@ -35,30 +38,40 @@ const ProjectList = () => {
   ];
   return (
     <div className={Styles.projects}>
+      <div className={"spacer spacer1"} />
       <h1>My Projects</h1>
+      <div className={"spacer spacer3"} />
       {Projects.map((Project) => (
-        <section
-          className={Styles.Project}
-          style={{ backgroundColor: Project.Color }}
-        >
-          <div className={Styles.ProjectWrapper}>
-            <div className={Styles.description}>
-              <h2 style={{ color: Project.TextColor }}>{Project.Name}</h2>
-              <a href={Project.Link} style={{ color: Project.TextColor }}>
-                Live Link
-              </a>
-              <ul className={Styles.Programs}>
-                {Project.Programs.map((Program) => (
-                  <li style={{ color: Project.TextColor }}> {Program.name}</li>
-                ))}
-              </ul>
-              <p style={{ color: Project.TextColor }}>{Project.Description}</p>
+        <>
+          <section
+            className={Styles.Project}
+            style={{ backgroundColor: Project.Color }}
+          >
+            <div className={Styles.ProjectWrapper}>
+              <div className={Styles.description}>
+                <h2 style={{ color: Project.TextColor }}>{Project.Name}</h2>
+                <a href={Project.Link} style={{ color: Project.TextColor }}>
+                  Live Link
+                </a>
+                <ul className={Styles.Programs}>
+                  {Project.Programs.map((Program) => (
+                    <li style={{ color: Project.TextColor }}>
+                      {" "}
+                      {Program.name}
+                    </li>
+                  ))}
+                </ul>
+                <p style={{ color: Project.TextColor }}>
+                  {Project.Description}
+                </p>
+              </div>
+              <div className={Styles["example-images"]}>
+                <img src={Project.img} />
+              </div>
             </div>
-            <div className={Styles["example-images"]}>
-              <img src={Project.img} />
-            </div>
-          </div>
-        </section>
+          </section>
+          <div className={Project.wave} />
+        </>
       ))}
     </div>
   );
