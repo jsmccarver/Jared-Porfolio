4484852d6fad400c72372c06573d1639799aacce
jsmccarv <jsmccarv@ncsu.edu>
2022-01-30 17:18:25 -0500

    Fix heroku bug

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
             <h3>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown
+              ever since the 1500 when an unknown
             </h3>
           </div>
           <div className={Styles.FeaturedProjects}>
